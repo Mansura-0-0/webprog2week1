c71d46462e92630adcce5f93ad777b9c7241935a
Mansura Khajra - STUDENT <s00265100@atu.ie>
2026-09-16 09:52:48 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, {Application, Request, Response} from "express" ;
+
+const PORT = process.env.PORT || 4000;
+
+const app: Application = express();
+
+app.get("/ping", async (_req : Request, res: Response) => {
+    res.json({
+    message: "hello from Mansura "
+    });
+});
+app.get('/bananas', async (_req : Request, res: Response) => {
+    res.json({
+    message: "this is bananas",
+    });
+});
+app.get('/Mangos', async (_req : Request, res: Response) => {
+    res.json({
+    message: "I love Mangos",
+    });
+});
+
+app.use((req, _res, next) => {  
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+    });
+
