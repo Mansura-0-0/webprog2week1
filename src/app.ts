3acb7fba211c6fc40eab268024d610196db1ad66
Mansura-0-0 <S00265100@atu.ie>
2026-09-17 16:58:43 +0100

    routes created

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-import express, {Application, Request, Response} from "express" ;
+import express, {Application, Request, Response} from "express";
+
+import carRoutes from './routes/cars';
 
 const PORT = process.env.PORT || 4000;
 
 const app: Application = express();
+
+app.use('/api/v1/cars', carRoutes); 
 
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
